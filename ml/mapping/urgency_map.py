71ecdfde22a6c95dc8c11841892b6280a60c3e5f
Lreed03 <Lreed0383@gmail.com>
2026-05-19 19:40:17 -0600

    Added a urgency map

---
+++ ml/mapping/urgency_map.py
@@ -0,0 +1,10 @@
+URGENCY_Map = {
+    "Common Cold": "Low",
+    "Flu": "Medium",
+    "Pneumonia": "High",
+    "Heart Attack": "Critical",
+    "Stroke": "Critical",
+    "Diabetes": "Medium",
+    "Hypertension": "Medium",
+    "Asthma": "Medium",
+}
